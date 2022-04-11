d8011f2e8b437093ba3c586e7db44983647f6f2b
pierpaolo <pierpaolo.albrici@gmail.com>
2022-04-11 11:30:37 +0200

    first commit

---
+++ CordARLibrary_TEST/src/main/java/com/example/cordarlibrary_test/CordAR.java
@@ -0,0 +1,21 @@
+package com.example.cordarlibrary_test;
+
+public class CordAR {
+    private String test;
+
+    public CordAR(String s){
+        this.test = s;
+    }
+
+    public void setTest(String s){
+        this.test = s;
+    }
+
+    public String getTest(){
+        return this.test; 
+    }
+
+    public String toString(){
+        return this.test;
+    }
+}
